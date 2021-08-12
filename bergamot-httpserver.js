30e4829ddfd516a85ab94eef7bfa1bf4e8fa0de0
Andre Natal <anatal@gmail.com>
2021-08-12 03:47:15 -0700

    Flag to skip ssl and serving index.html as root

--- bergamot-httpserver.js
+++ bergamot-httpserver.js
@@ -9,19 +9,32 @@ const fs = require('fs');
 const url = require('url');
 const nocache = require('nocache');
 const cors = require('cors');
+const path = require('path');
 
 let port = 8000;
 if (process.argv[2]) {
     port = process.argv[2];
 }
 
-let certpath = "/etc/letsencrypt";
+let skipssl = 0;
 if (process.argv[3]) {
-    certpath = process.argv[3];
+    skipssl = process.argv[3];
+}
+
+let certpath = "/etc/letsencrypt";
+if (process.argv[4]) {
+    certpath = process.argv[4];
 }
 
 app.use(cors())
 app.use(nocache());
+
+app.get('/', cors(), function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    res.header('Cross-Origin-Embedder-Policy','require-corp');
+    res.header('Cross-Origin-Opener-Policy','same-origin');
+});
+
 app.get('/*.*' , cors(), function(req, res) {
     var options = url.parse(req.url, true);
     var mime = Helper.getMime(options);
@@ -35,26 +48,22 @@ function serveFile(res, pathName, mime) {
             res.writeHead(500, {"Content-Type": "text/plain"});
             return res.end('Error loading ' + pathName + " with Error: " + err);
         }
-        res.header('Cross-Origin-Embedder-Policy','require-corp');
-        res.header('Cross-Origin-Opener-Policy','same-origin');
         res.writeHead(200, {"Content-Type": mime});
         res.end(data);
     });
 }
 
-
-https
-  .createServer(
-    {
-      key: fs.readFileSync(`${certpath}/privkey.pem`),
-      cert: fs.readFileSync(`${certpath}/cert.pem`),
-      ca: fs.readFileSync(`${certpath}/chain.pem`),
-    },
-    app
-  )
-  .listen(443, () => {
-    console.log('Listening https port 443')
-  })
+if (skipssl != 1){
+    https.createServer({
+            key: fs.readFileSync(`${certpath}/privkey.pem`),
+            cert: fs.readFileSync(`${certpath}/cert.pem`),
+            ca: fs.readFileSync(`${certpath}/chain.pem`),
+        },
+        app
+    ).listen(443, () => {
+        console.log('Listening https port 443')
+    })
+}
 
 server.listen(port);
 console.log(`HTTP and BinaryJS server started on port ${port}`);
