6c4aabe27bd66ba59f1f547f79f7c9b5ee938f50
evgeny pavlov <epavlov@mozilla.com>
2022-03-02 16:54:24 -0800

    Add more languages

--- js/index.js
+++ js/index.js
@@ -8,94 +8,99 @@ const langFrom = $("#lang-from");
 const langTo = $("#lang-to");
 
 const langs = [
-  ["en", "English"],
-  ["it", "Italian"],
-  ["pt", "Portuguese"],
-  ["ru", "Russian"],
-  ["cs", "Czech"],
-  ["de", "German"],
-  ["es", "Spanish"],
-  ["et", "Estonian"],
+    ["bg", "Bulgarian"],
+    ["cs", "Czech"],
+    ["en", "English"],
+    ["et", "Estonian"],
+    ["de", "German"],
+    ["is", "Icelandic"],
+    ["it", "Italian"],
+    ["nb", "Norwegian Bokmål"],
+    ["nn", "Norwegian Nynorsk"],
+    ["fa", "Persian"],
+    ["pt", "Portuguese"],
+    ["ru", "Russian"],
+    ["es", "Spanish"]
 ];
 
 if (window.Worker) {
-  worker = new Worker("js/worker.js");
-  worker.postMessage(["import"]);
+    worker = new Worker("js/worker.js");
+    worker.postMessage(["import"]);
 }
 
 document.querySelector("#input").addEventListener("keyup", function (event) {
-  translateCall();
+    translateCall();
 });
 
 const translateCall = () => {
-  const text = document.querySelector("#input").value + "  ";
-  if (!text.trim().length) return;
-  const paragraphs = text.split("\n");
-  $("#output").setAttribute("disabled", true);
-  const lngFrom = langFrom.value;
-  const lngTo = langTo.value;
-  worker.postMessage(["translate", lngFrom, lngTo, paragraphs]);
+    const text = document.querySelector("#input").value + "  ";
+    if (!text.trim().length) return;
+    const paragraphs = text.split("\n");
+    $("#output").setAttribute("disabled", true);
+    const lngFrom = langFrom.value;
+    const lngTo = langTo.value;
+    worker.postMessage(["translate", lngFrom, lngTo, paragraphs]);
 };
 
 worker.onmessage = function (e) {
-  if (e.data[0] === "translate_reply" && e.data[1]) {
-    document.querySelector("#output").value = e.data[1].join("\n\n");
-    $("#output").removeAttribute("disabled");
-  } else if (e.data[0] === "load_model_reply" && e.data[1]) {
-    status(e.data[1]);
-    translateCall();
-  } else if (e.data[0] === "import_reply" && e.data[1]) {
-    modelRegistry = e.data[1];
-    init();
-  }
+    if (e.data[0] === "translate_reply" && e.data[1]) {
+        document.querySelector("#output").value = e.data[1].join("\n\n");
+        $("#output").removeAttribute("disabled");
+    } else if (e.data[0] === "load_model_reply" && e.data[1]) {
+        status(e.data[1]);
+        translateCall();
+    } else if (e.data[0] === "import_reply" && e.data[1]) {
+        modelRegistry = e.data[1];
+        init();
+    }
 };
 
 langs.forEach(([code, name]) => {
-  langFrom.innerHTML += `<option value="${code}">${name}</option>`;
-  langTo.innerHTML += `<option value="${code}">${name}</option>`;
+    langFrom.innerHTML += `<option value="${code}">${name}</option>`;
+    langTo.innerHTML += `<option value="${code}">${name}</option>`;
 });
 
 const loadModel = () => {
-  const lngFrom = langFrom.value;
-  const lngTo = langTo.value;
-  if (lngFrom !== lngTo) {
-    status(`Installing model...`);
-    console.log(`Loading model '${lngFrom}${lngTo}'`);
-    worker.postMessage(["load_model", lngFrom, lngTo]);
-  } else {
-    const input = document.querySelector("#input").value;
-    document.querySelector("#output").value = input;
-  }
+    const lngFrom = langFrom.value;
+    const lngTo = langTo.value;
+    if (lngFrom !== lngTo) {
+        status(`Installing model...`);
+        console.log(`Loading model '${lngFrom}${lngTo}'`);
+        worker.postMessage(["load_model", lngFrom, lngTo]);
+    } else {
+        const input = document.querySelector("#input").value;
+        document.querySelector("#output").value = input;
+    }
 };
 
 langFrom.addEventListener("change", e => {
-  loadModel();
+    loadModel();
 });
 
 langTo.addEventListener("change", e => {
-  loadModel();
+    loadModel();
 });
 
 $(".swap").addEventListener("click", e => {
-  [langFrom.value, langTo.value] = [langTo.value, langFrom.value];
-  $("#input").value = $("#output").value;
-  loadModel();
+    [langFrom.value, langTo.value] = [langTo.value, langFrom.value];
+    $("#input").value = $("#output").value;
+    loadModel();
 });
 
 function init() {
-  // try to guess input language from user agent
-  let myLang = navigator.language;
-  if (myLang) {
-    myLang = myLang.split("-")[0];
-    let langIndex = langs.findIndex(([code]) => code === myLang);
-    if (langIndex > -1) {
-      console.log("guessing input language is", myLang);
-      langFrom.value = myLang;
+    // try to guess input language from user agent
+    let myLang = navigator.language;
+    if (myLang) {
+        myLang = myLang.split("-")[0];
+        let langIndex = langs.findIndex(([code]) => code === myLang);
+        if (langIndex > -1) {
+            console.log("guessing input language is", myLang);
+            langFrom.value = myLang;
+        }
     }
-  }
 
-  // find first output lang that *isn't* input language
-  langTo.value = langs.find(([code]) => code !== langFrom.value)[0];
-  // load this model
-  loadModel();
+    // find first output lang that *isn't* input language
+    langTo.value = langs.find(([code]) => code !== langFrom.value)[0];
+    // load this model
+    loadModel();
 }
